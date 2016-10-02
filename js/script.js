1c8728593a392c70c8cb01ee498088ab9c56d5d3
Andrei Dmytruk <mallboroks@mail.ru>
2016-10-03 01:15:43 +0300

    9

--- js/script.js
+++ js/script.js
@@ -3,5 +3,15 @@
 });*/
 
 $(function() {
-  $(function(){ $("select").uniform(); });
+  $(function(){ $("select").uniform();
+ });
+
+ var $links = $('.one_line_menue a');
+ $links.on('click', function (e) {
+   e.preventDefault();
+
+  var $nextLineMenue = $(this).siblings('.two_line_menue');
+     console.log($nextLineMenue);
+  $nextLineMenue.slideToggle();
+ })
 });
