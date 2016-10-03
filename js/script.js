d4dbbceff31371e951146433a68fee66cf306667
Andrei Dmytruk <mallboroks@mail.ru>
2016-10-04 00:10:22 +0300

    9

--- js/script.js
+++ js/script.js
@@ -6,10 +6,20 @@ $(function() {
   $(function(){ $("select").uniform();
  });
 
- var $links = $('.one_line_menue a');
- $links.mouseenter(function () {
+ /*var $links = $('.one_line_menue a');
+ $links.mouseover(function () {
   var $nextLineMenue = $(this).siblings('ul');
   $nextLineMenue.slideToggle();
+})*/
+
+var $links = $('.one_line_menue a');
+$links.mouseover(function () {
+ var $nextLineMenue = $(this).siblings('ul');
+ $nextLineMenue.slideDown();
+})
+var $ul = $('.one_line_menue ul');
+$ul.mouseleave(function () {
+  $ul.slideUp();
 })
 
-});
+})
